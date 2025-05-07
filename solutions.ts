d51cf4b621b79695f1efef416a137f33dd03116d
md-sazzadul <md.sazzadul.islam15@gmail.com>
2025-05-07 23:25:52 +0600

    add Vehicle and Car class

--- solutions.ts
+++ solutions.ts
@@ -11,3 +11,30 @@ function filterByRating(
 function concatenateArrays<T>(...arrays: T[][]): T[] {
   return arrays.reduce((acc, curr) => acc.concat(curr), []);
 }
+
+class Vehicle {
+  private make: string;
+  year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  getInfo() {
+    console.log(`Make: ${this.make}, Year: ${this.year}`);
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  getModel() {
+    console.log(`Model: ${this.model}`);
+  }
+}
