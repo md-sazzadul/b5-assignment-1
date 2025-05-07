29b0e45b72ef9880fff0e2373bd437353deab665
md-sazzadul <md.sazzadul.islam15@gmail.com>
2025-05-07 20:42:12 +0600

    write filterByRating function

--- solutions.ts
+++ solutions.ts
@@ -1,3 +1,9 @@
 function formatString(input: string, toUpper?: boolean): string {
   return toUpper ?? true ? input.toUpperCase() : input.toLowerCase();
 }
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
